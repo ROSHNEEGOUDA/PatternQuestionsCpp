e101e38bd1549343937067c586c43a02937215f5
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2023-08-03 21:16:47 +0530

    Update PatternQuestionsC++.cpp

--- PatternQuestionsC++.cpp
+++ PatternQuestionsC++.cpp
@@ -422,3 +422,54 @@ int main()
     }
     return 0;
 }
+
+//Alphabet Diamond pattern
+//    A
+//   ABC
+//  ABCDE
+// ABCDEFG
+//ABCDEFGHI
+// ABCDEFG
+// ABCDE
+//  ABC
+//   A
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+//to print upper triangle
+for(int line=1;line<=n;line++)
+{
+    int no_of_spaces=(n-line);
+    for(int k=0;k<no_of_spaces;k++)
+    {
+        cout<<" ";
+    }
+    int no_of_ch=2*line-1;
+    for(int j=0;j<no_of_ch;j++)
+        {
+            char ch=('A'+ j);
+            cout<<ch;
+        }
+        cout<<endl;
+}
+//loop to print lower triangle
+for(int line=n+1;line<2*n-1;line++)
+{
+    int no_of_spaces=(line-n);
+    for(int k=0;k<no_of_spaces;k++)
+    {
+        cout<<" ";
+    }
+    int no_char=2*(2*n-line)-1;
+    for(int j=0;j<no_char;j++)
+    {
+        char ch1=('A' + j);
+        cout<<ch1;
+    }
+    cout<<endl;
+}
+return 0;
+}
+
