7216df3dc7508f2d8846059235990fcb979ef522
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2023-08-05 09:07:25 +0530

    Update PatternQuestionsC++.cpp

--- PatternQuestionsC++.cpp
+++ PatternQuestionsC++.cpp
@@ -473,3 +473,27 @@ for(int line=n+1;line<2*n-1;line++)
 return 0;
 }
 
+//  *
+//  *
+//*****
+//  *
+//  *
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+for(int line=0;line<n;line++)
+{
+    for(int i=0;i<n;i++)
+    {
+       
+        if(i==n/2) cout<<"*";
+        else if(line==n/2) cout<<"*";
+        else cout<<" ";
+    }
+    cout<<endl;
+}
+return 0;
+}
+
